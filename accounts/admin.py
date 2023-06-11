038e7df31c2e027520502e475ab08dec513bd300
Matin.h.f81 <matinhosseini795@gmail.com>
2023-06-11 17:08:32 +0330

    Regsiter UserAdmin model for User model ✅

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+
+class UserAdmin(DefaultUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+
+admin.site.register(User, UserAdmin)
--- tests/testsample.py
+++ tests/testsample.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+from accounts.admin import UserAdmin
 from accounts.models import User
 from charities.models import Benefactor, Charity, Task
 
@@ -54,3 +55,13 @@ class TaskModelTest(TestCase):
         task = Task.objects.create(**self.task_info)
         task.refresh_from_db()
         self.assertIsNone(task.assigned_benefactor, '\nبرای فیلد assigned_benefactor مقدار null را برابر True قرار نداده‌اید.')
+
+
+
+class UserAdminTest(TestCase):
+    def test_credentials_section(self):
+        title = UserAdmin.fieldsets[0][0]
+        self.assertIsNone(title)
+        fields = list(UserAdmin.fieldsets[0][1].get('fields'))
+        expected_fields = ['username', 'password']
+        self.assertListEqual(fields, expected_fields)
