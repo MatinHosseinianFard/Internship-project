b1e23e6c4fc0b5f10fc2440d9f48d6a06389a491
Matin.h.f81 <matinhosseini795@gmail.com>
2023-06-11 16:48:24 +0330

    Create User model in account app ✅

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,12 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
-    pass
+    address = models.TextField(blank=True)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')), blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.username
