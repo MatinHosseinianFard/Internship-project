78fcc6a6c737b8f8383490f2bcbfba3888d824e6
Matin.h.f81 <matinhosseini795@gmail.com>
2023-06-11 16:55:49 +0330

    clean up User model 🧹

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
     address = models.TextField(blank=True)
     age = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True)
-    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')), blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
